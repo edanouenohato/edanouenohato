cdf478754b5446b5a7b9fc4201b91bb774f2b76e
Minami Tomonobu <minami@edanoue.com>
2023-12-05 11:35:29 +0900

    :zap: オプションを指定できるように
    Signed-off-by: Minami Tomonobu <minami@edanoue.com>

--- Assets/Scripts/Runtime/HeuUtilityEditorOnly.UpdateHoudiniRawMeshToGrayBoxSettings.cs
+++ Assets/Scripts/Runtime/HeuUtilityEditorOnly.UpdateHoudiniRawMeshToGrayBoxSettings.cs
@@ -3,14 +3,19 @@
 #nullable enable
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 internal partial class HeuUtilityEditorOnly
 {
 #if UNITY_EDITOR
 
-    private void UpdateHoudiniRawMeshToGrayBoxSettings()
+
+    private void UpdateHoudiniRawMeshToGrayBoxSettings(string args)
     {
+        // Gets the arguments
+        var argsArray = SplitArgs(args);
+
         // Change Name
         gameObject.name = "(Editor Only) Update Houdini Raw Mesh To GrayBox Settings";
 
@@ -35,6 +40,9 @@ internal partial class HeuUtilityEditorOnly
         // Null OBJ が書き出されがちなので, 名前決め打ちして削除する
         RemoveNullObj(hdaRoot);
 
+        // 特別なルールの HDA, Mesh / Collider 2つ Output がある GameObject を "いい感じに" 結合する
+        CombineMeshAndCollider(hdaRoot);
+
         // HDA にアクセスして, MeshRenderer がついている GameObject を起点とする
         var meshRenderers = hdaRoot.GetComponentsInChildren<MeshRenderer>();
 
@@ -46,6 +54,13 @@ internal partial class HeuUtilityEditorOnly
         foreach (var mr in meshRenderers)
         {
             var go = mr.gameObject;
+
+            // ついでにリネームしておく
+            var goName = go.name;
+            if (goName.Contains("_output0_output0_0"))
+            {
+                go.name = goName.Replace("_output0_output0_0", "");
+            }
 
             // Check1: IsStatic であるならスキップ
             if (go.isStatic)
@@ -69,16 +84,62 @@ internal partial class HeuUtilityEditorOnly
         }
 
         // 更新する
-        UpdateIsStatic(targetGameObject, true);
-        UpdateLayer(targetGameObject, "PHY_FrameCommon");
-        UpdateMaterial(targetGameObject,
-            "Packages/com.edanoue.eh.base/Common/Materials/DebugGrid VertexColor (World Space).mat");
+        UpdateIsStatic(targetGameObject, argsArray[0] == "on");
+        UpdateLayer(targetGameObject, argsArray[1]);
+        UpdateMaterial(targetGameObject, argsArray[2], argsArray[3]);
         AddMeshCollider(targetGameObject);
 
         // ログを出す
         foreach (var go in targetGameObject)
         {
             Debug.Log($"[HeuUtility] {go.name} settings auto update for GrayBox.", go);
+        }
+    }
+
+    /// <summary>
+    /// HDA で Output が2つあり, Mesh / Collider という名前のばあいは結合します
+    /// </summary>
+    /// <param name="hdaRoot"></param>
+    private static void CombineMeshAndCollider(Transform hdaRoot)
+    {
+        var targetColliderGo = new List<GameObject>();
+
+        // Note: ここらへん HDA の仕様変わったら変える必要があります
+        // 特別なルールの HDA, Mesh / Collider 2つ Output がある GameObject を "いい感じに" 結合する
+        foreach (Transform child in hdaRoot)
+        {
+            // _Collider_Collider_0 という名前を探す
+            if (child.gameObject.name.Contains("_Collider_Collider_0"))
+            {
+                targetColliderGo.Add(child.gameObject);
+            }
+        }
+
+        foreach (var colliderGo in targetColliderGo)
+        {
+            var baseName = colliderGo.name.Replace("_Collider_Collider_0", "");
+            var oldCollider = colliderGo.GetComponent<Collider>();
+
+            // Mesh の方を検索する
+            var targetMeshTrs = hdaRoot.Find(baseName + "_Mesh_Mesh_0");
+            if (targetMeshTrs == null)
+            {
+                continue;
+            }
+
+            // Mesh の方に Collider をコピーする
+            ComponentUtility.CopyComponent(oldCollider);
+            ComponentUtility.PasteComponentAsNew(targetMeshTrs.gameObject);
+
+            Debug.Log($"[HeuUtility] Founded Collider and Mesh pair. Combined: {baseName}", targetMeshTrs.gameObject);
+            // ついでにリネームしとく
+            targetMeshTrs.gameObject.name = baseName;
+        }
+
+        // Collider はもういらないので削除する
+        foreach (var colliderGo in targetColliderGo)
+        {
+            DestroyImmediate(colliderGo);
         }
     }
 
@@ -119,12 +180,28 @@ internal partial class HeuUtilityEditorOnly
         }
     }
 
-    private static void UpdateMaterial(in IReadOnlyList<GameObject> targetGos, string path)
+    private static void UpdateMaterial(in IReadOnlyList<GameObject> targetGos, string defaultMaterialPath,
+        string vertexColorMaterialPath)
     {
         foreach (var go in targetGos)
         {
+            // Renderer を取得しておく
             var renderer = go.GetComponent<MeshRenderer>();
-            renderer.sharedMaterial = AssetDatabase.LoadAssetAtPath<Material>(path);
+
+            // Mesh が VertexColor をもっているかどうかを確認する
+            var filter = go.GetComponent<MeshFilter>();
+            var mesh = filter.sharedMesh;
+            var vertexColor = mesh.colors;
+            if (vertexColor.Length == 0)
+            {
+                // VertexColor を持っていないなら, 通常のマテリアルを設定する
+                renderer.sharedMaterial = AssetDatabase.LoadAssetAtPath<Material>(defaultMaterialPath);
+            }
+            else
+            {
+                // VertexColor を持っているなら, 専用のマテリアルを設定する
+                renderer.sharedMaterial = AssetDatabase.LoadAssetAtPath<Material>(vertexColorMaterialPath);
+            }
         }
     }
 
@@ -132,6 +209,13 @@ internal partial class HeuUtilityEditorOnly
     {
         foreach (var go in targetGos)
         {
+            // 既になにかの Collider がついているなら無視する
+            if (go.GetComponent<Collider>() != null)
+            {
+                continue;
+            }
+
+            // MeshFilter が持つ SharedMesh を MeshCollider に設定する
             var filter = go.GetComponent<MeshFilter>();
             var col = go.AddComponent<MeshCollider>();
             col.sharedMesh = filter.sharedMesh;
